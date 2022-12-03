b43a506beb53ea897e5b95ae911be0045e3144f9
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-12-02 20:36:41 -0600

    feat: add change color of the seats when clicked

--- src/controler/ControlVistaAsientos.java
+++ src/controler/ControlVistaAsientos.java
@@ -6,7 +6,9 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 import model.Funcion;
 import model.Seat;
 import view.VistaAsientos;
@@ -15,36 +17,50 @@ public class ControlVistaAsientos implements ActionListener {
 
     private VistaAsientos view;
     private Seat seat;
+    private ArrayList<Seat> seats = new ArrayList<>();
+    private ArrayList<JButton> buttons = new ArrayList<>();
 
-    public ControlVistaAsientos(VistaAsientos view, Seat asiento) {
+    public ControlVistaAsientos(VistaAsientos view, ArrayList<Seat> seats) {
         this.view = view;
-        this.seat = asiento;
+        this.seats = seats;
         this.view.getjButton1().addActionListener(this);
         this.view.getjComboBox1().addActionListener(this);
+        initButtons();
         setSeatColor();
         configureComboBox();
+        addActionListenersToButtons();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (this.view.getjButton1() == e.getSource()) {
-            changeSeatStatus();
-            setSeatColor();
+        for (int i = 0; i <= this.buttons.size(); i++) {
+            try {
+                if (this.buttons.get(i) == e.getSource()) {
+                    changeSeatStatus(seats.get(i));
+                    setSeatColor();
+                }
+            } catch (IndexOutOfBoundsException er) {}
         }
     }
 
-    public void changeSeatStatus() {
-        String seatState = this.seat.getStatus();
+    public void changeSeatStatus(Seat seat) {
+        String seatState = seat.getStatus();
         if (seatState.equals("available")) {
-            this.seat.setStatus("selected");
+            seat.setStatus("selected");
         } else if (seatState.equals("selected")) {
-            this.seat.setStatus("available");
+            seat.setStatus("available");
         }
     }
 
     public void setSeatColor() {
-        Color color = this.seat.getStatusColor();
-        this.view.getjButton1().setBackground(color);
+        try {
+            for (int i = 0; i <= this.buttons.size(); i++) {
+                JButton currentButton = this.buttons.get(i);
+                Color currentColor = this.seats.get(i).getStatusColor();
+                currentButton.setBackground(currentColor);
+            }
+        } catch (IndexOutOfBoundsException e) {
+        }
     }
 
     public void configureComboBox() {
@@ -62,6 +78,44 @@ public class ControlVistaAsientos implements ActionListener {
     public ArrayList<String> requestPerformancesName() {
         DAOFunciones dao = new DAOFunciones();
         return dao.gerPerformancesName();
+    }
+
+    public void initButtons() {
+        //LOS BOTONES 28, 29 y 30 no son asientos. No inicializar.
+        this.buttons = new ArrayList<>(
+                Arrays.asList(
+                        this.view.getjButton3(),
+                        this.view.getjButton4(),
+                        this.view.getjButton5(),
+                        this.view.getjButton6(),
+                        this.view.getjButton7(),
+                        this.view.getjButton8(),
+                        this.view.getjButton9(),
+                        this.view.getjButton10(),
+                        this.view.getjButton11(),
+                        this.view.getjButton12(),
+                        this.view.getjButton13(),
+                        this.view.getjButton14(),
+                        this.view.getjButton15(),
+                        this.view.getjButton16(),
+                        this.view.getjButton17(),
+                        this.view.getjButton18(),
+                        this.view.getjButton19(),
+                        this.view.getjButton20(),
+                        this.view.getjButton21(),
+                        this.view.getjButton22(),
+                        this.view.getjButton23(),
+                        this.view.getjButton24(),
+                        this.view.getjButton25(),
+                        this.view.getjButton26(),
+                        this.view.getjButton27()
+                ));
+    }
+
+    public void addActionListenersToButtons() {
+        for (JButton button : this.buttons) {
+            button.addActionListener(this);
+        }
     }
 
 }
--- src/test/test_vista_asientos.java
+++ src/test/test_vista_asientos.java
@@ -1,6 +1,7 @@
 package test;
 
 import controler.ControlVistaAsientos;
+import java.util.ArrayList;
 import model.Seat;
 import view.VistaAsientos;
 
@@ -8,9 +9,13 @@ public class test_vista_asientos {
 
     public static void main(String[] args) {
         VistaAsientos vista = new VistaAsientos();
-        Seat asiento = new Seat();
+        ArrayList<Seat> seats = new ArrayList<>();
+        for(int i =0; i <= 26; i++){
+            Seat seat = new Seat();
+            seats.add(seat);
+        }
 
-        ControlVistaAsientos control = new ControlVistaAsientos(vista, asiento);
+        ControlVistaAsientos control = new ControlVistaAsientos(vista, seats);
         vista.setVisible(true);
     }
 
